f3ea329d9bfa345b1a889fabf72b14b9b5ab337c
Lili Boxer <ninaliboxer@gmail.com>
2019-04-24 14:02:02 -0700

    added event listener variables and function

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const flipButton = document.getElementById('flip-button');
+const headsChoice = document.getElementById('heads-choice');
+const coinImage = document.getElementById('coin-image');
+const winsDisplay = document.getElementById('wins');
+const lossesDisplay = document.getElementById('losses');
+const messageDisplay = document.getElementById('message');
+
+flipButton.addEventListener('click', () => {
+    
+});
