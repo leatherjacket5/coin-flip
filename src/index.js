43ef088d68d7134f78d8adec55bf266340f0317f
Lili Boxer <ninaliboxer@gmail.com>
2019-04-24 17:52:11 -0700

    removed bet function code

--- src/index.js
+++ src/index.js
@@ -7,25 +7,11 @@ const winsDisplay = document.getElementById('wins');
 const lossesDisplay = document.getElementById('losses');
 const messageDisplay = document.getElementById('message');
 
-
-// betting 
-
-const moneyDisplay = document.getElementById('money-display');
-const moneyBet = document.getElementById('money-bet');
-
-
-
 let wins = 0;
 let losses = 0;
 
-let moneyAmount = 1000;
-
-
-
-
 flipButton.addEventListener('click', () => {
 
-    
     const randomNumber = Math.random();
     const flip = getHeadsOrTails(randomNumber);
 
@@ -38,31 +24,21 @@ flipButton.addEventListener('click', () => {
     } 
 
     let guess = '';
+
     if(headsChoice.checked) {
         guess = 'heads';
     } else {
         guess = 'tails';
     }
 
-
     if(flip === guess) {
         wins++;
         winsDisplay.textContent = 'Wins: ' + wins;
         messageDisplay.textContent = 'You won!';
-        moneyAmount += moneyBet.value;
-        moneyDisplay.textContent = 'You have ' + moneyAmount;
     } else {
         losses++;
         lossesDisplay.textContent = 'Losses: ' + losses;
         messageDisplay.textContent = 'You suck!';
-        moneyAmount -= moneyBet.value;
-        moneyDisplay.textContent = 'You have ' + moneyAmount;
-
     }
-
-    console.log(moneyBet.value);
-    
-    
-  
 });
 
